d7ec662a6f921fea665c396809e36305e3c03fc4
Henry Kellam <henry.a.kellam@gmail.com>
2019-11-07 10:36:39 -0500

    added styles to review list item

--- client/src/components/ReviewListItem.js
+++ client/src/components/ReviewListItem.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+import './ReviewListItem.css'
+
 export default class ReviewListItem extends Component {
   render() {
     const { artist,
@@ -10,18 +12,21 @@ export default class ReviewListItem extends Component {
       id, } = this.props
     const link = `/${id}`
     return (
-      <div>
-        <div>
+      <div className='review-item-container'>
+
+        <div className='review-pic-container'>
           <a href={link}>
             <img src={image} />
           </a>
-        </div>
-        <h2>{title}</h2>
-        <h3>{artist}</h3>
-        <div>
+        </div >
+
+        <div className='review-item-info-container'>
+          <h2>{title}</h2>
+          <h3>{artist}</h3>
           <p>isLiked: {like ? 'yes' : 'no'}</p>
           <p># upPlays: {upPlay}</p>
         </div>
+
       </div>
     )
   }
