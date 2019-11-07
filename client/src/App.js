34de1b6876cb541733062c6cb445aaef7c98e35e
Henry Kellam <henry.a.kellam@gmail.com>
2019-11-07 09:35:51 -0500

    added a navigation bar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Home from './components/Home.js'
 import ReviewPage from './components/ReviewPage.js'
 import ReviewForm from './components/ReviewForm.js'
+import NavBar from './components/NavBar.js'
 
 
 import './App.css';
@@ -12,10 +13,7 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <nav>
-          <Link to='/'>Home</Link>
-          <Link to='/review'>Write a Review</Link>
-        </nav>
+        <NavBar />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/review" component={ReviewForm} />
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/review'>Write a Review</Link>
+      </nav>
+    )
+  }
+}
