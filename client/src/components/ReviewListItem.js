d730c98fd7da4b7fa05d63da5a7405ffddd3c0cc
Henry Kellam <henry.a.kellam@gmail.com>
2019-11-09 17:32:36 -0500

    added an UpPlay button with features

--- client/src/components/ReviewListItem.js
+++ client/src/components/ReviewListItem.js
@@ -1,21 +1,8 @@
 import React, { Component } from 'react'
-
+import StatusBar from './StatusBar.js'
 import './ReviewListItem.css'
-import axios from 'axios'
 
 export default class ReviewListItem extends Component {
-  deleteReview = async () => {
-    await axios.delete(`/api/review/${this.props.id}`)
-    this.props.refresh()
-  }
-  favoriteReview = async () => {
-    await axios.put(`/api/review/${this.props.id}`, { like: true })
-    this.props.refresh()
-  }
-  unfavoriteReview = async () => {
-    await axios.put(`/api/review/${this.props.id}`, { like: false })
-    this.props.refresh()
-  }
   render() {
     const { artist,
       title,
@@ -42,21 +29,11 @@ export default class ReviewListItem extends Component {
 
         <div className='icon-container'>
 
-          {like ?
-            <img onClick={this.unfavoriteReview}
-              src='/images/favorite-24px.svg' alt='favorite'
-              className='icon favorite' />
-            :
-            <img onClick={this.favoriteReview}
-              src='/images/favorite_border-24px.svg' alt='unfavorite'
-              className='icon unfavorite' />
-          }
-
-          <img src='/images/clear-24px.svg' alt='up play' />
-
-          <img onClick={this.deleteReview}
-            src='/images/clear-24px.svg' alt='clear'
-            className='icon delete' />
+          <StatusBar
+            like={like}
+            upPlay={upPlay}
+            refresh={this.props.refresh}
+            id={id} />
 
         </div>
 
