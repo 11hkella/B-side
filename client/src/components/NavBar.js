c91cc5a621c6b46bb6d3af1fc4a8e673a779708f
Henry Kellam <henry.a.kellam@gmail.com>
2019-11-10 14:26:11 -0500

    animated sidebar and logo

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,20 +6,46 @@ import Logo from './Logo.js'
 import './NavBar.css';
 
 export default class NavBar extends Component {
+  state = {
+    navBarOut: false,
+  }
+  toggleNavBar = () => {
+    this.setState({ navBarOut: !this.state.navBarOut })
+  }
   render() {
     return (
-      <div className='nav-bar-container'>
+      <div>
+        {this.state.navBarOut ?
+          <div className='nav-bar-container'>
 
-        <div className='nav-bar-logo-container'>
-          <Logo />
-        </div>
+            < div className='nav-bar-logo-container spinOut'
+              onClick={this.toggleNavBar} >
+              <Logo />
+            </div >
 
-        <nav>
-          <h1>B sides</h1>
-          <Link to='/'>Home</Link>
-          <Link to='/review'>Write a Review</Link>
-        </nav>
+            <nav className='navShow'>
+              <h1>B sides</h1>
+              <Link to='/'>Home</Link>
+              <Link to='/review'>Write a Review</Link>
+            </nav>
 
+          </div >
+          :
+          <div className='nav-bar-container nav-bar-resize'>
+
+            <div className='nav-bar-logo-container spinIn'
+              onClick={this.toggleNavBar} >
+              <Logo />
+            </div>
+
+            <nav className='navHide'>
+              <h1>B sides</h1>
+              <Link to='/'>Home</Link>
+              <Link to='/review'>Write a Review</Link>
+            </nav>
+
+          </div>
+        }
       </div>
     )
   }
