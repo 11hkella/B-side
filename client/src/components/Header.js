a25180133cc58422eb7f09f4061d3e8acd5991b9
Henry Kellam <henry.a.kellam@gmail.com>
2019-11-11 00:13:10 -0500

    updated font and styled header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,7 +6,7 @@ export default class Header extends Component {
   render() {
     return (
       <div className='header-container'>
-        <h1>Title</h1>
+        <h1>NEWEST<br />REVIEWS</h1>
       </div>
     )
   }
