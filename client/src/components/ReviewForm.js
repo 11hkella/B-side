5413878c509874b5eb750c0d89a6cffa3236237b
Henry Kellam <henry.a.kellam@gmail.com>
2020-02-17 14:02:54 -0500

    dotenv installed

--- client/src/components/ReviewForm.js
+++ client/src/components/ReviewForm.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import { Redirect } from 'react-router-dom'
+import firebaseBucket from '../Firebase';
+
 
 import './ReviewForm.css'
 
-import * as firebase from 'firebase';
-const firebaseConfig = {
-  apiKey: "AIzaSyDLK0fHDMP9Jce3ifZ1agoAxwoeqxP67rQ",
-  authDomain: "b-side-review.firebaseapp.com",
-  databaseURL: "https://b-side-review.firebaseio.com",
-  projectId: "b-side-review",
-  storageBucket: "gs://b-side-review.appspot.com",
-  messagingSenderId: "99363105763",
-  appId: "1:99363105763:web:fadb2aafd230a9896a8ed6",
-  measurementId: "G-8ZESD6MGSQ"
-};
-firebase.initializeApp(firebaseConfig);
-const ref = firebase.storage().ref();
-
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDLK0fHDMP9Jce3ifZ1agoAxwoeqxP67rQ",
+//   authDomain: "b-side-review.firebaseapp.com",
+//   databaseURL: "https://b-side-review.firebaseio.com",
+//   projectId: "b-side-review",
+//   storageBucket: "gs://b-side-review.appspot.com",
+//   messagingSenderId: "99363105763",
+//   appId: "1:99363105763:web:fadb2aafd230a9896a8ed6",
+//   measurementId: "G-8ZESD6MGSQ"
+// };
+// firebase.initializeApp(firebaseConfig);
+// const ref = firebase.storage().ref();
+const ref = firebaseBucket.ref
+console.log(ref)
 
 export default class ReviewForm extends Component {
   state = {
@@ -52,6 +54,7 @@ export default class ReviewForm extends Component {
       try {
         //upload image
         const uploadTask = await ref.child(imagePath).put(this.state.imageFile, { contentType: this.state.imageFile.type });
+        //get firebase url
         const downloadURL = await uploadTask.ref.getDownloadURL();
         this.setState({
           imagePath: downloadURL,
